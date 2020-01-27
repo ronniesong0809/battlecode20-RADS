a8fcfee87357560a03f26edb176ef5ba5c3b8a79
Danc2050 <danc2@pdx.edu>
2020-01-27 14:39:05 -0800

    moving HQ to proper location

--- src/team4player/HQ.java
+++ src/team4player/HQ.java
@@ -34,6 +34,16 @@ public class HQ extends Building {
             bc.broadcastInitialWallComplete();
             sentWallComplete = true;
         }
+        Team enemy = rc.getTeam().opponent();
+        RobotInfo[] enemiesInRange = rc.senseNearbyRobots(GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED, enemy);
+        for (RobotInfo e : enemiesInRange) {
+            if (e.type == RobotType.DELIVERY_DRONE) {
+                if (rc.canShootUnit(e.ID)) {
+                    rc.shootUnit(e.ID);
+                    System.out.println("I shoot'ed enemy! " + e.type);
+                }
+            }
+        }
     }
 
     public void initSurroundingWalls() {
--- src/team4player/Netgun.java
+++ src/team4player/Netgun.java
@@ -2,12 +2,21 @@ package team4player;
 import battlecode.common.*;
 
 public class Netgun extends Building {
-    public Netgun(RobotController r) {
-        super(r);
+    public Netgun(RobotController rc) {
+        super(rc);
     }
 
     public void takeTurn() throws GameActionException {
         super.takeTurn();
-        //TODO:
+        Team enemy = rc.getTeam().opponent();
+        RobotInfo[] enemiesInRange = rc.senseNearbyRobots(GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED, enemy);
+        for (RobotInfo e : enemiesInRange) {
+            if (e.type == RobotType.DELIVERY_DRONE) {
+                if (rc.canShootUnit(e.ID)) {
+                    rc.shootUnit(e.ID);
+                    System.out.println("I shoot'ed enemy! " + e.type);
+                }
+            }
+        }
     }
 }
