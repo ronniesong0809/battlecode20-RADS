4914dbe8d2c9d738b530620e36c19acda03e18f4
Daniel Connelly <Danc2@pdx.edu>
2020-02-07 12:46:26 -0800

    Diagonal search (#15)
    * Build design school only around HQ
    
    * miners move diagonally instead of randomly and don't get stuck!
    
    * cleanup of code
    
    * cleanup of code(2)

--- src/team4player/Miner.java
+++ src/team4player/Miner.java
@@ -1,10 +1,15 @@
 package team4player;
 import battlecode.common.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class Miner extends Unit{
     static int numDesignSchool = 0;
     static int numRefinery = 0;
+		int diagonalDir = -1; // the diagonal direction a miner is heading if nowhere else to go.
+		int [] diagonalArr = {1,3,5,7}; // diagonal directions to move
     ArrayList<MapLocation> soupLocations = new ArrayList<MapLocation>();
 
     public Miner(RobotController rc) {
@@ -23,12 +28,10 @@ public class Miner extends Unit{
 		}
 
 		public void buildDesignSchoolOrRefinery() throws GameActionException{
-        // Build design school
-        if (numDesignSchool < 1 && !senseBuilding(RobotType.DESIGN_SCHOOL)) {
-            if (tryBuild(RobotType.DESIGN_SCHOOL, Util.randomDirection())) {
-                numDesignSchool++;
-                System.out.println("build a Design School");
-            }
+        // Build design school if miner hasn't made one, none are nearby, and we are by HQ  --- all to control production of DSs
+        if (numDesignSchool < 1 && !senseBuilding(RobotType.DESIGN_SCHOOL) && senseBuilding(RobotType.HQ) && tryBuild(RobotType.DESIGN_SCHOOL)){
+						numDesignSchool++;
+						System.out.println("built a Design School");
         }
 				else if(!senseBuilding(RobotType.REFINERY)) tryBuild(RobotType.REFINERY);
 		}
@@ -36,19 +39,30 @@ public class Miner extends Unit{
 		public void checkForSoup() throws GameActionException{
 				MapLocation[] soup = rc.senseNearbySoup(-1);
 				if (soup != null && soup.length != 0) { // we found soup! Head towards it
-						int randomLoc = (int) Math.random() * soup.length + 0; // random soup to avoid crowds
-						walkTowardsSoup(soup, randomLoc);
+						boolean mined = false; // checks if  we need to move areas for soup
 						for (Direction dir : Util.directions){
-								tryMine(dir);
+								if(tryMine(dir)){mined = true;}
 						}
+						if (!mined){
+							int randomLoc = (int) (Math.random() * soup.length + 0); // random soup to avoid crowds
+						  walkTowardsSoup(soup, randomLoc);}
 				} else {
-					System.out.println("GOING RANDOM DIRECTION"); // we can be stuck
-					nav.goTo(Util.randomDirection());
+					System.out.println("GOING DIAGONAL DIRECTION"); // we can be stuck
+					if(!nav.goTo(Util.directions[diagonalDir])) {
+						nav.goTo(Util.randomDirection());
+						diagonalDir=-1;}
+					//System.out.println("GOING RANDOM DIRECTION"); // we can be stuck
+					//nav.goTo(Util.randomDirection());
 				}
 		}
 
     public void takeTurn() throws GameActionException {
         super.takeTurn();
+				if (diagonalDir == -1){
+					int random = (int) (Math.random() * 4); // random soup to avoid crowds
+					diagonalDir = diagonalArr[random];
+				}
+				else{System.out.println(Util.directions[diagonalDir]);}
 				int x = 0;
 				if (rc.getSoupCarrying() >= 70) x=1;
 				switch(x){
@@ -78,23 +92,25 @@ public class Miner extends Unit{
 					buildDesignSchoolOrRefinery();
 					MapLocation refineryLocation = findRefinery();
 					while (rc.getSoupCarrying() >= 70) {
-							System.out.println("at soup limit");
+							System.out.println("TRYING TO DEPOSIT SOUP...");
+							for (Direction dir : Util.directions)
+									if(tryRefine(dir)){ System.out.println("SUCCESFULLY DEPOSITED SOUP"); return;}
 							if (refineryLocation != null) {
 									while(true){
 											System.out.println("Toward to Refinery!");
-											if (nav.goTo(refineryLocation) == false) { break; }
+											if (nav.goTo(refineryLocation) == false) {
+													nav.goTo(Util.randomDirection()); // must randomly move so as to avoid getting stuck. TODO -- add N,E,W, S exclusively?
+												  break; }
 									}
 							}
 							else{
 									while(true){
 											System.out.println("Toward to HQ!");
-											if(nav.goTo(hqLoc) == false){ break;}
+											if(nav.goTo(hqLoc) == false){
+												nav.goTo(Util.randomDirection());
+												break;}
 									}
 							}
-							System.out.println("TRYING TO DEPOSIT SOUP...");
-							for (Direction dir : Util.directions)
-									if(tryRefine(dir)){ System.out.println("SUCCESFULLY DEPOSITED SOUP"); break;}
-							nav.goTo(Util.randomDirection());
 					}
     }
 
