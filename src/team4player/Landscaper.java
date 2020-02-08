6ff835859fce57ad22473a711157129041cb97a9
Alex-Saber <saber@pdx.edu>
2020-02-07 20:11:38 -0800

    Adds a bit of landscaper code

--- src/team4player/Landscaper.java
+++ src/team4player/Landscaper.java
@@ -8,11 +8,8 @@ public class Landscaper extends Unit{
     static int digX = 0;
     static int digY = 0;
     static int minWallHeight = 10;
-    static int maxWallHeight = 100;
-    static boolean initialWallComplete = false;
-    static float chanceNewWall = 0.005f;
-    static int round = 0;
     static MapLocation wallSpot = null;
+    static int personality = 0;
 
     public Landscaper(RobotController rc){
         super(rc);
@@ -21,81 +18,33 @@ public class Landscaper extends Unit{
     public void takeTurn() throws GameActionException {
         super.takeTurn();
 
-        round++;
-
+        // Initialize the map locations to build on
         if (wallLocs == null) {
             initializeWallLocationsAndLevels();
         }
 
-        // Figure out digging pattern:
-        if (hqLoc.x % 2 == 0) {
-            digX = 1;
-        }
-        if (hqLoc.y % 2 == 0) {
-            digY = 1;
-        }
-
-        int currWallSpot = 0;
-        // Find next spot to dig to
-
-        for (MapLocation tileToCheck : wallLocs) {
-            currWallSpot++;
-            if (rc.getLocation().distanceSquaredTo(tileToCheck) < 4 && rc.canDepositDirt(rc.getLocation().directionTo(tileToCheck))) {
-                if (rc.senseElevation(tileToCheck) < minWallHeight) {
-                    wallSpot = tileToCheck;
-                    break;
-                }
-            }
-        }
-
-        // Gather dirt from appropriate spots
-        if (rc.getDirtCarrying() == 0) {
-            tryDig();
-        }
-
-        // Add more spots to deposit dirt to in lattice fashion.
-
-        if (wallSpot != null && rc.canDepositDirt(rc.getLocation().directionTo(wallSpot))) {
-            if (rc.senseElevation(wallSpot) <= minWallHeight) {
-                rc.depositDirt(rc.getLocation().directionTo(wallSpot));
-            }
-
-            int newWallHeight = bc.readInitialWallComplete();
-            if (newWallHeight > minWallHeight) {
-                minWallHeight = newWallHeight;
-            }
-            if (initialWallComplete || bc.readInitialWallComplete() == maxWallHeight) {
-                initialWallComplete = true;
-                System.out.println("Initial wall complete! ******************");
-                if (rc.senseElevation(wallSpot) >= maxWallHeight) {
-                    boolean shouldStop = false;
-                    for (int i = 0; i < 8; i++) {
-                        MapLocation newLoc = wallSpot.add(Direction.values()[i]);
-                        if (!wallLocs.contains(newLoc) &&
-                                !(newLoc.x % 2 == digX && newLoc.y % 2 == digY) &&
-                                newLoc.distanceSquaredTo(hqLoc) > 2 &&
-                                rc.senseElevation(newLoc) <= minWallHeight) {
-                            wallLocs.add(wallSpot.add(Direction.values()[i]));
-                            shouldStop = true;
-                        }
-                    }
-                    wallLocs.remove(wallSpot);
-                    if (shouldStop) {
-                        wallSpot = null;
-                    }
-                }
-            }
-        }
-
-        // Either go to wall spot or go in a random direction.
-        if (wallSpot != null) {
-            nav.goTo(wallSpot);
-        } else if (rc.getLocation().distanceSquaredTo(hqLoc) > 30) {
-            nav.goTo(hqLoc);
+        // Find wall spot
+        if (wallSpot == null) {
+            findWallSpot();
         }
         else {
-            nav.goTo(Util.randomDirection());
+            // Try to dig dirt if you are carrying none
+            if (rc.getDirtCarrying() == 0) {
+                tryDig();
+            }
+            // Otherwise deposit dirt if you are close enough
+            else if (rc.getLocation().distanceSquaredTo(wallSpot) > 0) {
+                nav.goTo(wallSpot);
+            }
+            else {
+                tryDeposit(wallSpot);
+            }
         }
+    }
+
+
+    public void runHqWallBuilding() {
+
     }
 
     boolean tryDig() throws GameActionException {
@@ -108,7 +57,6 @@ public class Landscaper extends Unit{
             }
         }
 
-
         if (dir != null && rc.canDigDirt(dir)) {
             rc.digDirt(dir);
             return true;
@@ -116,45 +64,100 @@ public class Landscaper extends Unit{
         return false;
     }
 
+    public void tryDeposit(MapLocation spot) throws GameActionException {
+        if (spot != null && rc.canDepositDirt(rc.getLocation().directionTo(spot))) {
+            rc.depositDirt(rc.getLocation().directionTo(spot));
+        }
+    }
+
     public void initializeWallLocationsAndLevels() {
+        // Figure out digging pattern:
+        if (hqLoc.x % 2 == 0) {
+            digX = 1;
+        }
+        if (hqLoc.y % 2 == 0) {
+            digY = 1;
+        }
+
         wallLocs = new ArrayList<MapLocation>();
         wallLevels = new ArrayList<Integer>();
 
-        int block_x = hqLoc.x - 2;
-        int block_y = hqLoc.y + 2;
+        int block_x = hqLoc.x - 1;
+        int block_y = hqLoc.y + 1;
         for (int i = 0; i < 16; i++) {
             wallLocs.add(new MapLocation(block_x, block_y));
 
             switch (i) {
                 case 0:
                 case 1:
-                case 2:
-                case 3:
                     block_x++;
                     break;
                 case 4:
                 case 5:
-                case 6:
-                case 7:
                     block_y--;
                     break;
                 case 8:
                 case 9:
-                case 10:
-                case 11:
                     block_x--;
                     break;
                 case 12:
                 case 13:
-                case 14:
-                case 15:
                     block_y++;
                     break;
             }
         }
 
+
+    }
+
+    public void senseWallHeights() {
         for (int i = 0; i < wallLocs.size(); i++) {
             wallLevels.add(3);
         }
     }
+
+    public MapLocation findWallSpot() throws GameActionException {
+        for (MapLocation tileToCheck : wallLocs) {
+            if (rc.getLocation().distanceSquaredTo(tileToCheck) < 4 && rc.canDepositDirt(rc.getLocation().directionTo(tileToCheck))) {
+//                if (rc.senseElevation(tileToCheck) < minWallHeight) {
+//                    return tileToCheck;
+//                }
+                return tileToCheck;
+            }
+        }
+        return null;
+    }
+
+//    public void digLattice() {
+//        if (wallSpot != null && rc.canDepositDirt(rc.getLocation().directionTo(wallSpot))) {
+//            if (rc.senseElevation(wallSpot) <= minWallHeight) {
+//                rc.depositDirt(rc.getLocation().directionTo(wallSpot));
+//            }
+//            int newWallHeight = bc.readInitialWallComplete();
+//            if (newWallHeight > minWallHeight) {
+//                minWallHeight = newWallHeight;
+//            }
+//            if (initialWallComplete || bc.readInitialWallComplete() == maxWallHeight) {
+//                initialWallComplete = true;
+//                System.out.println("Initial wall complete! ******************");
+//                if (rc.senseElevation(wallSpot) >= maxWallHeight) {
+//                    boolean shouldStop = false;
+//                    for (int i = 0; i < 8; i++) {
+//                        MapLocation newLoc = wallSpot.add(Direction.values()[i]);
+//                        if (!wallLocs.contains(newLoc) &&
+//                                !(newLoc.x % 2 == digX && newLoc.y % 2 == digY) &&
+//                                newLoc.distanceSquaredTo(hqLoc) > 2 &&
+//                                rc.senseElevation(newLoc) <= minWallHeight) {
+//                            wallLocs.add(wallSpot.add(Direction.values()[i]));
+//                            shouldStop = true;
+//                        }
+//                    }
+//                    wallLocs.remove(wallSpot);
+//                    if (shouldStop) {
+//                        wallSpot = null;
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
