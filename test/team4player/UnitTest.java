cd1bb85b132204d04b3ed15b63ef1fba47dd7fdb
Ronnie Song <ronniesong0809@gmail.com>
2020-02-12 23:56:43 -0800

    refactor: fix senseNearbyRobots() return value

--- test/team4player/UnitTest.java
+++ test/team4player/UnitTest.java
@@ -24,28 +24,33 @@ public class UnitTest {
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Before
-    public void setup() throws GameActionException {
-        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{});
+    public void setup() {
         when(rcMock.getTeam()).thenReturn(Team.A);
         when(rcMock.getType()).thenReturn(RobotType.HQ);
     }
 
     @Test
     public void findHQ() throws GameActionException {
+        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{new RobotInfo(12, Team.A, RobotType.HQ, 0, false, 0, 0, 0, new MapLocation(5, 5))});
+
         unitMock.findHQ();
         verify(rcMock).senseNearbyRobots();
-        verify(rcMock, times(0)).getTeam();
+        verify(rcMock).getTeam();
     }
 
     @Test
     public void findRefineryTest() throws GameActionException {
+        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{new RobotInfo(12, Team.A, RobotType.REFINERY, 0, false, 0, 0, 0, new MapLocation(10, 10))});
+
         MapLocation result = unitMock.findRefinery();
-        assertEquals(result, null);
+        assertEquals(result, new MapLocation(10, 10));
     }
 
     @Test
     public void nearbyRobotTest() throws GameActionException {
+        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{new RobotInfo(12, Team.A, RobotType.HQ, 0, false, 0, 0, 0, new MapLocation(5, 5))});
+
         boolean result = unitMock.nearbyRobot(RobotType.HQ);
-        assertEquals(result, false);
+        assertEquals(result, true);
     }
 }
