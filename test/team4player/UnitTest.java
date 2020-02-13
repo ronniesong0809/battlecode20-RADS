e7b5887e2dda49a081bbe42fab62fea9494e1aaf
Ronnie Song <ronniesong0809@gmail.com>
2020-02-12 23:17:03 -0800

    refactor: fix Unit testing cases, and get coverage

--- test/team4player/UnitTest.java
+++ test/team4player/UnitTest.java
@@ -1,42 +1,51 @@
 package team4player;
 
-import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
-import battlecode.common.RobotType;
+import battlecode.common.*;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class UnitTest {
     @Mock
-    Unit unitMock = mock(Unit.class);
+    RobotController rcMock = mock(RobotController.class);
+
+    @InjectMocks
+    Unit unitMock = new Unit(rcMock);
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Before
-    public void setUp() throws GameActionException {
-        when(unitMock.findRefinery()).thenReturn(new MapLocation(5, 5));
-        when(unitMock.nearbyRobot(RobotType.MINER)).thenReturn(true);
+    public void setup() throws GameActionException {
+        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{});
+        when(rcMock.getTeam()).thenReturn(Team.A);
+        when(rcMock.getType()).thenReturn(RobotType.HQ);
+    }
+
+    @Test
+    public void findHQ() throws GameActionException {
+        unitMock.findHQ();
+        verify(rcMock).senseNearbyRobots();
+        verify(rcMock, times(0)).getTeam();
     }
 
     @Test
     public void findRefineryTest() throws GameActionException {
         MapLocation result = unitMock.findRefinery();
-        assertEquals(result, new MapLocation(5, 5));
+        assertEquals(result, null);
     }
 
     @Test
     public void nearbyRobotTest() throws GameActionException {
-        boolean result = unitMock.nearbyRobot(RobotType.MINER);
-        assertEquals(result, true);
+        boolean result = unitMock.nearbyRobot(RobotType.HQ);
+        assertEquals(result, false);
     }
 }
