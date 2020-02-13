aa0c4127d7515789ac92e5cacd34412d6c5eb10c
Ronnie Song <ronniesong0809@gmail.com>
2020-02-12 22:44:54 -0800

    refactor: fix Unit testing cases, and get coverage

--- src/team4player/Robot.java
+++ src/team4player/Robot.java
@@ -1,4 +1,5 @@
 package team4player;
+
 import battlecode.common.*;
 
 public class Robot {
@@ -12,7 +13,7 @@ public class Robot {
         bc = new Broadcast(rc);
     }
 
-    public void takeTurn() throws GameActionException{
+    public void takeTurn() throws GameActionException {
         turnCount += 1;
         //System.out.println("I'm a robot");
     }
@@ -21,30 +22,31 @@ public class Robot {
      * Attempts to build a given robot in a given direction.
      *
      * @param type The type of the robot to build
-     * @param dir The intended direction of movement
+     * @param dir  The intended direction of movement
      * @return true if a move was performed
      * @throws GameActionException
      */
-		// Daniel's overloaded function (builds in all directions instead of one)
+    // Daniel's overloaded function (builds in all directions instead of one)
     boolean tryBuild(RobotType type, MapLocation hqLoc) throws GameActionException {
         MapLocation currentLocation = rc.getLocation();
-				for (Direction dir : Util.directions){ // build in all directions, since we don't care
-					MapLocation potentialBuildlocation = currentLocation.add(dir);
-					if (rc.isReady() && rc.canBuildRobot(type, dir) && potentialBuildlocation.distanceSquaredTo(hqLoc) > 8){
-							rc.buildRobot(type, dir);
-							return true;
-					}
-				}
-				return false;
+        for (Direction dir : Util.directions) { // build in all directions, since we don't care
+            MapLocation potentialBuildlocation = currentLocation.add(dir);
+            int distance = potentialBuildlocation.distanceSquaredTo(hqLoc);
+            if (rc.isReady() && rc.canBuildRobot(type, dir) && distance > 8) {
+                rc.buildRobot(type, dir);
+                return true;
+            }
+        }
+        return false;
     }
 
     boolean tryBuild(RobotType type, Direction dir) throws GameActionException {
-			  //TODO -- add build on HQ wall.
-				if (rc.isReady() && rc.canBuildRobot(type, dir)) {
-						rc.buildRobot(type, dir);
-						return true;
-				}
-				return false;
+        //TODO -- add build on HQ wall.
+        if (rc.isReady() && rc.canBuildRobot(type, dir)) {
+            rc.buildRobot(type, dir);
+            return true;
+        }
+        return false;
     }
 
 
