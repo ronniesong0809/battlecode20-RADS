a97bec38573f2775ed4cf4062b7dc4c9b1eced67
Ronnie Song <ronniesong0809@gmail.com>
2020-02-29 02:41:26 -0800

    test: fix the landscaper and its unit tests (#67)
    landscapers are no longer explodes

--- test/team4player/LandScaperTest.java
+++ test/team4player/LandScaperTest.java
@@ -6,13 +6,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class LandScaperTest {
@@ -22,31 +21,29 @@ public class LandScaperTest {
     RobotController rcMock = mock(RobotController.class);
     @Mock
     Navigation navMock = mock(Navigation.class);
-    @Mock
-    Broadcast bcMock = mock(Broadcast.class);
-    @Mock
-    Unit unitMock = new Unit(rcMock);
 
     @InjectMocks
     Landscaper lpMock = new Landscaper(rcMock);
 
     @Before
-    public void setup() {
+    public void setup() throws GameActionException {
+        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{new RobotInfo(12, Team.A, RobotType.HQ, 0, false, 0, 0, 0, new MapLocation(5, 5))});
         when(rcMock.getTeam()).thenReturn(Team.A);
-        when(rcMock.getType()).thenReturn(RobotType.HQ);
+        lpMock.findHQ();
     }
 
     @Test
-    public void takeTurnTest() throws GameActionException{
-        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{new RobotInfo(12, Team.A, RobotType.HQ, 0, false, 0, 0, 0, new MapLocation(5, 5))});
-        when(rcMock.getLocation()).thenReturn(new MapLocation(1,1));
+    public void takeTurnTest() throws GameActionException {
+        when(rcMock.getLocation()).thenReturn(new MapLocation(1, 1));
+        when(navMock.goAround(new MapLocation(5, 5))).thenReturn(true);
         lpMock.takeTurn();
     }
 
     @Test
-    public void takeTurnTest2() throws GameActionException{
-        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{new RobotInfo(12, Team.A, RobotType.HQ, 0, false, 0, 0, 0, new MapLocation(5, 5))});
-        when(rcMock.getLocation()).thenReturn(new MapLocation(5,4));
+    public void takeTurnTest2() throws GameActionException {
+        when(rcMock.getLocation()).thenReturn(new MapLocation(5, 4));
+        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{null});
+        initializeWallLocationsAndLevels();
         lpMock.takeTurn();
     }
 
@@ -69,9 +66,9 @@ public class LandScaperTest {
     }
 
     @Test
-    public void findDigSpotTest() throws GameActionException {
+    public void findDigSpotTest() {
         createDigLocations();
-        when(rcMock.getLocation()).thenReturn(new MapLocation(8,8));
+        when(rcMock.getLocation()).thenReturn(new MapLocation(8, 8));
         when(rcMock.canDigDirt(Direction.NORTH)).thenReturn(true);
 
         Direction result = lpMock.findDigSpot();
@@ -79,9 +76,9 @@ public class LandScaperTest {
     }
 
     @Test
-    public void findDigSpotTest2() throws GameActionException {
+    public void findDigSpotTest2() {
         createDigLocations();
-        when(rcMock.getLocation()).thenReturn(new MapLocation(8,8));
+        when(rcMock.getLocation()).thenReturn(new MapLocation(8, 8));
         when(rcMock.canDigDirt(Direction.WEST)).thenReturn(true);
         Direction result = lpMock.findDigSpot();
 
@@ -90,55 +87,42 @@ public class LandScaperTest {
 
     @Test
     public void tryDepositTest() throws GameActionException {
-        when(unitMock.hqLoc()).thenReturn(new MapLocation(5,5));
-        when(unitMock.hqLocy()).thenReturn(5);
-        when(unitMock.hqLocx()).thenReturn(5);
-        when(unitMock.hqLocy()).thenReturn(5);
         when(rcMock.canDepositDirt(Direction.NORTH)).thenReturn(true);
-        when(rcMock.getLocation()).thenReturn(new MapLocation(5,5));
+        when(rcMock.getLocation()).thenReturn(new MapLocation(5, 5));
         lpMock.initializeWallLocationsAndLevels();
 
-        lpMock.tryDeposit(new MapLocation(4,7)); // SOUTHEAST
-        lpMock.tryDeposit(new MapLocation(4,3)); // NORTHEAST
-        lpMock.tryDeposit(new MapLocation(7,4)); // NORTHWEST
-        lpMock.tryDeposit(new MapLocation(7,6)); // SOUTHWEST
-        for (int i = 0; i<301; i++){
+        lpMock.tryDeposit(new MapLocation(4, 7)); // SOUTHEAST
+        lpMock.tryDeposit(new MapLocation(4, 3)); // NORTHEAST
+        lpMock.tryDeposit(new MapLocation(7, 4)); // NORTHWEST
+        lpMock.tryDeposit(new MapLocation(7, 6)); // SOUTHWEST
+        for (int i = 0; i < 301; i++) {
             takeTurnTest();
         }
-        lpMock.tryDeposit(new MapLocation(8,7)); // SOUTHWEST
+        lpMock.tryDeposit(new MapLocation(8, 7)); // SOUTHWEST
     }
 
     @Test
     public void createDigLocations() {
-        when(unitMock.hqLocx()).thenReturn(5);
-        when(unitMock.hqLocy()).thenReturn(5);
         lpMock.createDigLocations();
     }
 
     @Test
-    public void initializeWallLocationsAndLevels(){
-        when(unitMock.hqLocx()).thenReturn(5);
-        when(unitMock.hqLocy()).thenReturn(5);
+    public void initializeWallLocationsAndLevels() {
         lpMock.initializeWallLocationsAndLevels();
-        when(unitMock.hqLocx()).thenReturn(4);
-        when(unitMock.hqLocy()).thenReturn(4);
+        when(rcMock.senseNearbyRobots()).thenReturn(new RobotInfo[]{new RobotInfo(12, Team.A, RobotType.HQ, 0, false, 0, 0, 0, new MapLocation(4, 4))});
         lpMock.initializeWallLocationsAndLevels();
     }
 
     @Test
     public void senseWallHeights() {
-        when(unitMock.hqLocx()).thenReturn(5);
-        when(unitMock.hqLocy()).thenReturn(5);
         lpMock.initializeWallLocationsAndLevels();
         lpMock.senseWallHeights();
     }
 
     @Test
-    public void findWallSpotTestReturnNull() throws GameActionException{
-        when(unitMock.hqLocx()).thenReturn(5);
-        when(unitMock.hqLocy()).thenReturn(5);
+    public void findWallSpotTestReturnNull() throws GameActionException {
         lpMock.initializeWallLocationsAndLevels();
-        when(rcMock.getLocation()).thenReturn(new MapLocation(40,5));
+        when(rcMock.getLocation()).thenReturn(new MapLocation(40, 5));
         when(rcMock.getCurrentSensorRadiusSquared()).thenReturn(2);
 
         MapLocation result = lpMock.findWallSpot();
@@ -146,15 +130,13 @@ public class LandScaperTest {
     }
 
     @Test
-    public void findWallSpotTestReturnMapLocation() throws GameActionException{
-        when(unitMock.hqLocx()).thenReturn(5);
-        when(unitMock.hqLocy()).thenReturn(5);
+    public void findWallSpotTestReturnMapLocation() throws GameActionException {
         lpMock.initializeWallLocationsAndLevels();
-        when(rcMock.getLocation()).thenReturn(new MapLocation(4,6));
+        when(rcMock.getLocation()).thenReturn(new MapLocation(4, 6));
         when(rcMock.getCurrentSensorRadiusSquared()).thenReturn(5);
-        when(rcMock.senseRobotAtLocation(new MapLocation(4,4))).thenReturn(null);
+        when(rcMock.senseRobotAtLocation(new MapLocation(4, 4))).thenReturn(null);
 
         MapLocation result = lpMock.findWallSpot();
-        assertEquals(result, new MapLocation(4,6));
+        assertEquals(result, new MapLocation(4, 6));
     }
 }
